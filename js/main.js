448d25845f1d85a1646d18778d45014c27edcfc4
alexanderKw <alkrivulya@gmail.com>
2017-05-12 02:14:43 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,21 +1,45 @@
-(function() {
+'use strict';
+
+(function () {
   'use strict';
-  const sections = document.querySelectorAll('.section');
-  const navList = document.querySelector('#nav-list');
-  const pointer = document.querySelector('.pointer');
+
+  var sections = document.querySelectorAll('.section');
+  var navList = document.querySelector('#nav-list');
+  var pointer = document.querySelector('.pointer');
 
   function getScroll() {
-    sections.forEach(el => {
-      let top = el.offsetTop - 300;
-      let bottom = top + el.clientHeight;
-      let scroll = window.pageYOffset;
-      let id = el.getAttribute('id');
+    sections.forEach(function (el) {
+      var top = el.offsetTop - 300;
+      var bottom = top + el.clientHeight;
+      var scroll = window.pageYOffset;
+      var id = el.getAttribute('id');
       if (scroll > top && scroll < bottom) {
-        for (let item of navList.children) {
-          item.classList.remove('active');
-          if (item.firstChild.getAttribute('href') == `#${id}`) {
-            item.classList.add('active');
-            pointer.style.left = item.offsetLeft + item.offsetWidth / 2 + 'px';
+        var _iteratorNormalCompletion = true;
+        var _didIteratorError = false;
+        var _iteratorError = undefined;
+
+        try {
+          for (var _iterator = navList.children[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+            var item = _step.value;
+
+            item.classList.remove('active');
+            if (item.firstChild.getAttribute('href') == '#' + id) {
+              item.classList.add('active');
+              pointer.style.left = item.offsetLeft + item.offsetWidth / 2 + 'px';
+            }
+          }
+        } catch (err) {
+          _didIteratorError = true;
+          _iteratorError = err;
+        } finally {
+          try {
+            if (!_iteratorNormalCompletion && _iterator.return) {
+              _iterator.return();
+            }
+          } finally {
+            if (_didIteratorError) {
+              throw _iteratorError;
+            }
           }
         }
       }
@@ -24,13 +48,13 @@
 
   window.addEventListener('scroll', getScroll);
 
-  navList.addEventListener('click', e => {
-    if (e.target.getAttribute('href') == `#${id}`) {
+  navList.addEventListener('click', function (e) {
+    if (e.target.getAttribute('href') == '#' + id) {
       window.scrollTo(0, el.offsetTop - 150);
     }
   });
 
-  sections.forEach(s => {
+  sections.forEach(function (s) {
     s.style.height = window.innerHeight + 'px';
     s.style.minHeight = 800 + 'px';
   });
