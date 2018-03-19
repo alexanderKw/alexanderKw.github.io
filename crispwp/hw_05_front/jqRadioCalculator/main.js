efc4946b109394000c2629e4daded10cc95d1a66
alexanderKw <alkrivulya@gmail.com>
2018-03-19 02:14:42 +0200

    added task "jqRadioCalculator"

---
+++ crispwp/hw_05_front/jqRadioCalculator/main.js
@@ -0,0 +1,46 @@
+'use strict';
+
+$(document).ready(function() {
+  $('.buttons').on('click', '.btn', function() {
+    var btnVal = $(this).val();
+
+    var field1 = $('.field-1');
+    var field2 = $('.field-2');
+
+    var num1 = parseFloat(field1.val());
+    var num2 = parseFloat(field2.val());
+
+    var result = 0;
+    var success = false;
+
+    if ($.isNumeric(num1) && $.isNumeric(num2)) {
+      switch (btnVal) {
+        case '+':
+          result = num1 + num2;
+          success = true;
+          break;
+        case '-':
+          result = num1 - num2;
+          success = true;
+          break;
+        case '*':
+          result = num1 * num2;
+          success = true;
+          break;
+        case '/':
+          result = num1 / num2;
+          success = true;
+          break;
+      }
+    } else {
+      result = 'Оба значения должны быть числами!';
+    }
+
+    $('.result').val(result);
+
+    if (success) {
+      field1.val('');
+      field2.val('');
+    }
+  });
+});
