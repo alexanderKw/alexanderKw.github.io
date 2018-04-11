4dde8ecf89e4d1a69a5470355634b7004aa79f97
alexanderKw <alkrivulya@gmail.com>
2018-04-12 00:36:05 +0300

    deleted Cs files

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# Getting started
-Click [this link](https://alexanderKw.github.io) to start the page
+# GitHub Pages
--- crispwp/hw_05_front/jqUpperCase/main.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-$(document).ready(function() {
-  $('.wrapper').on('click', '.btn-up-c', function() {
-    var text = $('.text-field');
-    text.val(text.val().toUpperCase());
-  });
-});
